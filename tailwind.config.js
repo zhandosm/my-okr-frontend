53a3dfe869acaee9308656b84d26d446e2f3dfeb
zhandosm <zhanmdd@gmail.com>
2022-02-16 04:44:41 +0900

    Setup tailwincss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      moblack: '#111317',
+      mogreen: '#25A77A',
+      molightgrey: '#F6F7F8',
+      mowhite: '#FFFFFF', 
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
