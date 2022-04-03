1a670ef4143fabcf3fab283205bfbc34828663e4
Zhandos Mukatayev <zhandee@Zhandoss-MacBook-Pro.local>
2022-04-03 16:09:42 +0900

    Update middleware and validating authorization

---
+++ pages/signup/_middleware.ts
@@ -0,0 +1,5 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { noAuth } from '../../middlewares';
+
+export const middleware = async (req: NextRequest, res: NextResponse) => await noAuth(req, res);
