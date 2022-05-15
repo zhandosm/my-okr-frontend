d7dbcfbf404928d00c1cbbb2e06ac6cd166900aa
zhandosm <zhanmdd@gmail.com>
2022-05-15 22:52:05 +0900

    Add useObjectives hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './useProjects'
+export * from './useObjectives'
--- src/hooks/useObjectives/index.ts
+++ src/hooks/useObjectives/index.ts
@@ -1,13 +1,13 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { useQuery } from 'react-query'
 
-export const fetchObjectives = async (projectId:string) => {
+export const fetchObjectives = async (projectId:string | string[] | undefined) => {
     const axiosConfig:AxiosRequestConfig = { withCredentials: true };
-    const response = await axios.get(`${process.env.API_HOST}/projects`, axiosConfig);
+    const response = await axios.get(`${process.env.API_HOST}/projects/${projectId}`, axiosConfig);
     const { data } = response;
-    return data;
+    return data.objectives;
 }
 
-export const useObjectives = (projectId:string) => {
-    return useQuery(['objectives'], () => fetchObjectives(projectId))
+export const useObjectives = (projectId:string | string[] | undefined) => {
+    return useQuery(['objectives', { projectId: projectId }], () => fetchObjectives(projectId), { enabled: !!projectId })
 }
