95c3deef950dca585c4df14f2cb9e44cbccc85be
zhandosm <zhanmdd@gmail.com>
2022-05-15 22:09:49 +0900

    Set common layout routing. Fix lottie loader. remove unnecesarry code

--- src/hooks/useObjectives/index.ts
+++ src/hooks/useObjectives/index.ts
@@ -1,13 +1,13 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { useQuery } from 'react-query'
 
-export const fetchProjects = async () => {
+export const fetchObjectives = async (projectId:string) => {
     const axiosConfig:AxiosRequestConfig = { withCredentials: true };
     const response = await axios.get(`${process.env.API_HOST}/projects`, axiosConfig);
     const { data } = response;
     return data;
 }
 
-export const useProjects = () => {
-    return useQuery(['projects'], () => fetchProjects())
+export const useObjectives = (projectId:string) => {
+    return useQuery(['objectives'], () => fetchObjectives(projectId))
 }
--- src/pages/myokr/[projectId]/index.tsx
+++ src/pages/myokr/[projectId]/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from 'next';
 import { CommonLayout } from '@components';
-import { useRouter } from 'next/router';
 
 const Page: NextPage = () => {
     return <CommonLayout>
       Current Project ID
       Key results List
     </CommonLayout>
-  )
 }
 
 export default Page
