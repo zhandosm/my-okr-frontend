1806ab057eb2ded0285b3ca0e7f4159c234288b9
zhandosm <zhanmdd@gmail.com>
2022-05-26 23:39:43 +0900

    Drag and drop setup

--- src/components/scrumBoard/BoardColumn.tsx
+++ src/components/scrumBoard/BoardColumn.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
 
 export interface ToDoObj {
     keyResultId: string;
@@ -13,7 +14,13 @@ export interface ToDoObj {
 
 interface BoardColumnDataObj {
     status: string;
-    toDos: ToDoObj[];
+    toDos: ToDoObj[]; 
+}
+
+interface ToDoProps {
+    title: string;
+    id: string;
+    index: number;
 }
 
 interface StatusTitleMap {
@@ -28,13 +35,34 @@ const statusTitleMap:StatusTitleMap = {
     '2': 'Done'
 }
 
+
+export const ToDo: FunctionComponent<ToDoProps>  = ({id, index, title}) => { 
+    return <Draggable draggableId={id} index={index}>
+        {(provided)=><div
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            className="my-2 p-2 bg-molightgrey rounded-lg cursor-pointer">
+                {title}
+            </div>}
+    </Draggable>
+}
+
 export const BoardColumn: FunctionComponent<BoardColumnDataObj>  = ({ toDos, status }) => {
     return <div className="bg-mowhite rounded-lg p-3.5 w-1/3 md:w-full">
         <h5 className='font-medium'>{statusTitleMap[status]}</h5>
-        <div className=''>
-            {toDos.map((toDoObj, i)=>{
-                return <div key={i} className="my-2 p-2 bg-molightgrey rounded-lg cursor-pointer hover:text-mogreen active:drop-shadow transition-all">{toDoObj.title}</div>
-            })}
-        </div>
+        <Droppable droppableId={status}>
+           {(provided)=><div ref={provided.innerRef} {...provided.droppableProps} className=''>
+                {toDos.map((toDoObj, i)=>{
+                    return <ToDo
+                        id={toDoObj._id}
+                        key={i}
+                        index={i}
+                        title={toDoObj.title}
+                    />
+                })}
+                {provided.placeholder}
+            </div>}
+        </Droppable>
     </div>
 };
--- src/components/scrumBoard/index.tsx
+++ src/components/scrumBoard/index.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
 import { ToDoObj, BoardColumn } from './BoardColumn';
 
 export interface ClassifiedToDos {
@@ -12,9 +13,14 @@ interface BoardDataObj {
 }
 
 export const ScrumBoard: FunctionComponent<BoardDataObj>  = ({ boardData }) => {
+    const onDragEnd = (result:any) => {
+      console.log(result)
+    }
     return <div className="h-full w-full flex justify-between space-x-4">
-       <BoardColumn status="0" toDos={boardData['0']}/>
-       <BoardColumn status="1" toDos={boardData['1']}/>
-       <BoardColumn status="2" toDos={boardData['2']}/>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <BoardColumn key={0} status="0" toDos={boardData['0']}/>
+        <BoardColumn key={1} status="1" toDos={boardData['1']}/>
+        <BoardColumn key={2} status="2" toDos={boardData['2']}/>
+      </DragDropContext>
     </div>
 };
