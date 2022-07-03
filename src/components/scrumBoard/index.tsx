c27519cc1b09fb8b4d1eb71dd00696616dc04a04
zhandosm <zhanmdd@gmail.com>
2022-07-03 17:57:09 +0900

    Add mutation to drag and drop

--- src/components/scrumBoard/index.tsx
+++ src/components/scrumBoard/index.tsx
@@ -2,7 +2,8 @@ import React, { FunctionComponent } from 'react';
 import { DragDropContext } from 'react-beautiful-dnd';
 import { updateToDoRequest } from '@hooks';
 import { ToDoObj, BoardColumn } from './BoardColumn';
-import { useQueryClient, QueryClient } from 'react-query';
+import { useQueryClient, useMutation } from 'react-query';
+import { useRouter } from 'next/router';
 
 export interface ClassifiedToDos {
   0: ToDoObj[];
@@ -14,26 +15,75 @@ interface BoardDataObj {
     boardData: ClassifiedToDos
 }
 
+const filterToDoList = (list:[], newToDoId:string, oldToDoRef:any) => {
+  return list.filter((toDoObj:any)=> {
+    if(toDoObj._id===newToDoId){
+      oldToDoRef = toDoObj;
+      console.log(toDoObj)
+      return false;
+    }else{
+      return true;
+    }
+  })
+}
+
 export const ScrumBoard: FunctionComponent<BoardDataObj>  = ({ boardData }) => {
   const queryClient = useQueryClient();
-  console.log(queryClient)
-    const onDragEnd = async (result:any) => {
-      const { destination, source, draggableId: toDoId } = result;
-      if(!destination) return;
-      if(destination.droppableId===source.droppableId) return;
-      const body = { status: destination.droppableId };
-      console.log(body)
-      updateToDoRequest(toDoId, body)
-        .then(
-          data => queryClient.refetchQueries()//console.log(useQueryClient)
-        )
-        .catch(err=>console.log(err));
+  const router = useRouter();
+  
+  const { mutate: updateToDo  } = useMutation(
+    updateParams => updateToDoRequest(updateParams.toDoId, updateParams.body),
+    {
+        onMutate: (updatedToDo) => {
+            queryClient.setQueryData(['toDos', { keyResultId: router.query.keyResultId }], (prev) => {
+                const data = prev;
+                let oldToDo = null;
+                data[0] = data[0].filter((toDoObj:any)=> {
+                  if(toDoObj._id===updatedToDo.toDoId){
+                    oldToDo = toDoObj;
+                    return false;
+                  }else{
+                    return true;
+                  }
+                });
+                data[1] = data[1].filter((toDoObj:any)=> {
+                  if(toDoObj._id===updatedToDo.toDoId){
+                    oldToDo = toDoObj;
+                    return false;
+                  }else{
+                    return true;
+                  }
+                });
+                data[2] = data[2].filter((toDoObj:any)=> {
+                  if(toDoObj._id===updatedToDo.toDoId){
+                    oldToDo = toDoObj;
+                    return false;
+                  }else{
+                    return true;
+                  }
+                });
+                oldToDo.status = updatedToDo.body.status;
+                data[updatedToDo.body.status] = [...data[updatedToDo.body.status], oldToDo]
+                return data;
+            });
+        },
+        onError: () => alert("Something went wrong while moving item")
     }
-    return <div className="h-full w-full flex justify-between space-x-4">
-      <DragDropContext onDragEnd={onDragEnd}>
-        <BoardColumn key={0} status="0" toDos={boardData['0']}/>
-        <BoardColumn key={1} status="1" toDos={boardData['1']}/>
-        <BoardColumn key={2} status="2" toDos={boardData['2']}/>
-      </DragDropContext>
-    </div>
+);
+  
+  const onDragEnd = async (result:any) => {
+    const { destination, source, draggableId: toDoId } = result;
+    if(!destination) return;
+    if(destination.droppableId===source.droppableId) return;
+    const body = { status: destination.droppableId };
+    const updateParams = { toDoId, body };
+    updateToDo(updateParams);
+  }
+  return <div className="h-full w-full flex justify-between space-x-4">
+    <DragDropContext onDragEnd={onDragEnd}>
+      <BoardColumn key={0} status="0" toDos={boardData['0']}/>
+      <BoardColumn key={1} status="1" toDos={boardData['1']}/>
+      <BoardColumn key={2} status="2" toDos={boardData['2']}/>
+    </DragDropContext>
+  </div>
 };
